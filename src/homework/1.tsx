1c69cb390744de09a4eb567faab70ca8b6ced92c
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2024-03-18 15:14:10 +0200

    work

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,40 +1,53 @@
+//У вас є компонент React, який використовує useRef та IntersectionObserver для визначення, коли користувач переглядає кінець вмісту. Ваше завдання полягає в наступному:
+
+//Встановіть правильні типи пропсів для цього компонента. У ньому є дві властивості: children і onContentEndVisible. children - це будь-який валідний React вузол, а onContentEndVisible - це функція без аргументів, що повертає void.
+
+//Встановіть правильний тип useRef. Посилання endContentRef використовується для div, який міститься в кінці вмісту.
+
+//Встановіть правильний тип для options (клас також може бути типом для options).
+
 import React, { useEffect, useRef } from 'react';
+
+interface Props {
+   children: React.ReactNode;
+   onContentEndVisible: () => void;
+}
 
 // Опишіть Props
 export function Observer({ children, onContentEndVisible }: Props) {
-  // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
+   const endContentRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0,
-      root: null,
-    };
+   useEffect(() => {
+      // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
+      const options: IntersectionObserverInit = {
+         rootMargin: '0px',
+         threshold: 1.0,
+         root: null,
+      };
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.intersectionRatio > 0) {
-          onContentEndVisible();
-          observer.disconnect();
-        }
-      });
-    }, options);
+      const observer = new IntersectionObserver((entries) => {
+         entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+               onContentEndVisible();
+               observer.disconnect();
+            }
+         });
+      }, options);
 
-    if (endContentRef.current) {
-      observer.observe(endContentRef.current);
-    }
+      if (endContentRef.current) {
+         observer.observe(endContentRef.current);
+      }
 
-    return () => {
-      observer.disconnect();
-    };
-  }, [onContentEndVisible]);
+      return () => {
+         observer.disconnect();
+      };
+   }, [onContentEndVisible]);
 
-  return (
-    <div>
-      {children}
-      <div ref={endContentRef} />
-    </div>
-  );
+   return (
+      <div>
+         {children}
+         <div ref={endContentRef} />
+      </div>
+   );
 }
--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,91 +1,105 @@
-import React, { createContext, useMemo, useState, useContext } from "react";
-import noop from "lodash/noop";
+//Ви вирішили застосувати до меню контекст і тепер вам потрібно його типізувати.
 
-type MenuIds = "first" | "second" | "last";
+//Описати тип SelectedMenu: Це має бути об'єкт, який містить id з типом MenuIds
+
+//Описати тип MenuSelected: Цей тип є об'єктом, що містить selectedMenu
+
+//Описати тип MenuAction: Цей тип являє собою об'єкт з методом onSelectedMenu, який приймає об'єкт типу SelectedMenu як аргумент повертає void.
+
+//Описати тип PropsProvider: Опишіть правильний тип для дітей
+
+//Описати тип PropsMenu: Опишіть тип для menus, він має бути від типу Menu
+
+import React, { createContext, useMemo, useState, useContext } from 'react';
+import noop from 'lodash/noop';
+
+type MenuIds = 'first' | 'second' | 'last';
 type Menu = { id: MenuIds; title: string };
 
 // Додати тип Menu Selected
+type SelectedMenu = { id: MenuIds };
+type MenuSelected = { selectedMenu: SelectedMenu };
 
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+   selectedMenu: { id: 'first' },
 });
 
 // Додайте тип MenuAction
+type MenuAction = { onSelectedMenu: (SelectedMenu: SelectedMenu) => void };
 
 const MenuActionContext = createContext<MenuAction>({
-  onSelectedMenu: noop,
+   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+   children: React.ReactNode; // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
-  // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+   // Додати тип для SelectedMenu він повинен містити { id }
+   const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({ id: 'first' });
 
-  const menuContextAction = useMemo(
-    () => ({
-      onSelectedMenu: setSelectedMenu,
-    }),
-    []
-  );
+   const menuContextAction = useMemo(
+      () => ({
+         onSelectedMenu: setSelectedMenu,
+      }),
+      []
+   );
 
-  const menuContextSelected = useMemo(
-    () => ({
-      selectedMenu,
-    }),
-    [selectedMenu]
-  );
+   const menuContextSelected = useMemo(
+      () => ({
+         selectedMenu,
+      }),
+      [selectedMenu]
+   );
 
-  return (
-    <MenuActionContext.Provider value={menuContextAction}>
-      <MenuSelectedContext.Provider value={menuContextSelected}>
-        {children}
-      </MenuSelectedContext.Provider>
-    </MenuActionContext.Provider>
-  );
+   return (
+      <MenuActionContext.Provider value={menuContextAction}>
+         <MenuSelectedContext.Provider value={menuContextSelected}>
+            {children}
+         </MenuSelectedContext.Provider>
+      </MenuActionContext.Provider>
+   );
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+   menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
-  const { onSelectedMenu } = useContext(MenuActionContext);
-  const { selectedMenu } = useContext(MenuSelectedContext);
+   const { onSelectedMenu } = useContext(MenuActionContext);
+   const { selectedMenu } = useContext(MenuSelectedContext);
 
-  return (
-    <>
-      {menus.map((menu) => (
-        <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
-          {menu.title}{" "}
-          {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
-        </div>
-      ))}
-    </>
-  );
+   return (
+      <>
+         {menus.map(menu => (
+            <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
+               {menu.title} {selectedMenu.id === menu.id ? 'Selected' : 'Not selected'}
+            </div>
+         ))}
+      </>
+   );
 }
 
 export function ComponentApp() {
-  const menus: Menu[] = [
-    {
-      id: "first",
-      title: "first",
-    },
-    {
-      id: "second",
-      title: "second",
-    },
-    {
-      id: "last",
-      title: "last",
-    },
-  ];
+   const menus: Menu[] = [
+      {
+         id: 'first',
+         title: 'first',
+      },
+      {
+         id: 'second',
+         title: 'second',
+      },
+      {
+         id: 'last',
+         title: 'last',
+      },
+   ];
 
-  return (
-    <MenuProvider>
-      <MenuComponent menus={menus} />
-    </MenuProvider>
-  );
+   return (
+      <MenuProvider>
+         <MenuComponent menus={menus} />
+      </MenuProvider>
+   );
 }
